d90d5d94f89d3f1db1e2b5831f2046f4a45472c7
Williams Hernández <hernandw@gmail.com>
2026-03-30 20:20:50 -0300

    :art: estilos a las pagonas

--- src/controller/courseController.js
+++ src/controller/courseController.js
@@ -2,7 +2,9 @@
 
 export const home = (req, res)=>{
     try {
-        res.render('home')
+        res.render('home',{
+            
+        })
     } catch (error) {
         console.log(error)
         res.status(500).send('Error al cargar el Servidor', error)
